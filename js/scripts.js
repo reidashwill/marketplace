70ea58a04d863eef569e5f8b8e5cd01946c98175
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-03-25 15:59:24 -0700

    debug script
    co-authored-by: Reid-Ashwill <reidashwill@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-var userName = $("input#userName").val();
-var address1 = $("input#address1").val();
-var aptNum = $("input#aptNum").val();
-var address2 = $("input#address2").val();
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
+    event.preventDefault();
+    var userName = $("input#userName").val();
+    var address1 = $("input#address1").val();
+    var aptNum = $("input#aptNum").val();
+    var address2 = $("input#address2").val();
+    var purchase = $("#beverage").val();
  
-
-
-$(document).ready(function(){
-  $("#formOne").submit(function(event){
     $(".userName").text(userName);
     $(".address1").text(address1);
     $(".aptNum").text(aptNum);
     $(".address2").text(address2);
-   
-    event.preventDefault();
+    $(".purchase").text(purchase);
+    $("#receipt").show();
   });
 }); 
